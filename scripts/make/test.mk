e3baea7b168a81f1e2de075d2a1f48d6d437ee4e
mihir jain <mihirjain.work@gmail.com>
2021-07-22 16:09:01 +0530

    a

--- scripts/make/test.mk
+++ scripts/make/test.mk
@@ -13,7 +13,7 @@ exec-test: ## Execute test suite
 	# docker-compose -f docker-compose.test.yml exec nodejs_test ng test
 
 stop-test: ## Stop docker containers (test)
-	docker-compose -f docker-compose.test.yml stop
+	docker-compose -f docker-compose.test.yml stop $(service)_test
 
 clean-test: stop-test ## Stop docker containers, clean data and workspace (test)
 	docker-compose -f docker-compose.test.yml down -v --remove-orphans --rmi all
