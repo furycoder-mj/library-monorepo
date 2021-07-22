f75b1541483d066178ef71f06e734de4c90949f1
mihir jain <mihirjain.work@gmail.com>
2021-07-22 16:40:36 +0530

    a

--- scripts/make/test.mk
+++ scripts/make/test.mk
@@ -4,7 +4,7 @@ build-test: ## Build docker image (test)
 	docker-compose -f docker-compose.test.yml build
 
 start-test:build-test ## Build and start docker containers (test)
-	docker-compose -f docker-compose.test.yml up -d
+	docker-compose -f docker-compose.test.yml up $(service)_test -d
 	docker container ls -la
 
 exec-test: ## Execute test suite
@@ -16,6 +16,7 @@ stop-test: ## Stop docker containers (test)
 	docker-compose -f docker-compose.test.yml stop $(service)_test
 
 clean-test: stop-test ## Stop docker containers, clean data and workspace (test)
-	docker-compose -f docker-compose.test.yml down -v --remove-orphans --rmi all
+	docker-compose -f docker-compose.test.yml down $(service)_test -v 
+# --remove-orphans --rmi all
 
-test: start-test exec-test clean-test ## Run test suite
+test: start-test exec-test #clean-test ## Run test suite
