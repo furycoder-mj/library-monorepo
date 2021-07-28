14993ba12cf4fcb31f88e0a8a09883d08f4ab904
mihir jain <mihirjain.work@gmail.com>
2021-07-28 23:24:04 +0530

    pipeline major fix

--- scripts/make/build.mk
+++ scripts/make/build.mk
@@ -1,7 +1,13 @@
 .PHONY: build build-prod
 
 build: ## Build docker image
-	docker-compose build
+	docker-compose -f docker-compose.dev.yml build -q $(service)
 
-build-prod: ## Build docker image (production)
-	docker-compose -f docker-compose.prod.yml build
+push: ## Push docker image
+	docker-compose -f docker-compose.dev.yml push $(service)
+
+logout: ## docker logout
+	docker logout
+
+build-push: build push logout## Build docker image (production)
+
--- scripts/make/test.mk
+++ scripts/make/test.mk
@@ -1,16 +1,10 @@
-.PHONY: build-test test-up test-down test
+.PHONY: test-up test-down test
 
-build-test: ## Build docker image (test)
-	docker-compose -f docker-compose.test.yml build
-
-start-test:build-test ## Build and start docker containers (test)
-	docker-compose -f docker-compose.test.yml up -d $(service)_test 
-	docker container ls -la
+start-test: ## Build and start docker containers (test)
+	docker-compose -f docker-compose.test.yml up -d $(service)_test
 
 exec-test: ## Execute test suite
 	docker-compose -f docker-compose.test.yml exec -T $(service)_test go test -v -vet=off -coverprofile=/tmp/coverage.out ./...
-	# docker-compose -f docker-compose.test.yml exec nodejs_test ng lint
-	# docker-compose -f docker-compose.test.yml exec nodejs_test ng test
 
 stop-test: ## Stop docker containers (test)
 	docker-compose -f docker-compose.test.yml stop $(service)_test
